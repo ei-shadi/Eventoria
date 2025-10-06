a147eba926b12079fa913d6a974a6c47b13b8669
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-10-06 21:52:03 +0600

    organizer route api is fixed

--- src/app/api/(Organizers)/organizerEvents/route.js
+++ src/app/api/(Organizers)/organizerEvents/route.js
@@ -2,56 +2,47 @@ import { getDatabase } from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
-
-
-export async function GET(req){
-
-    const db = await getDatabase() ;
-    const { searchParams } = new URL(req.url) ;
-    const email = searchParams.get('email') ;
-    const result = await db.collection('events').find({organizerEmail : email,approvalStatus: "approved"}).toArray() ;
-export async function POST(request){
-
-    const db = await getDatabase() ;
-    const data = await request.json() ;
-    const result = await db.collection('events').insertOne(data) ;
-    return NextResponse.json(result) ;
-
+export async function GET(req) {
+  const db = await getDatabase();
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
+  const result = await db
+    .collection("events")
+    .find({ organizerEmail: email, approvalStatus: "approved" })
+    .toArray();
+  return NextResponse.json(result);
 }
 
-export async function PUT(req){
-
-    const db = await getDatabase() ;
-    const { searchParams } = new URL(req.url) ;
-    const id = searchParams.get('eventId') ;
-    const data = await req.json() ;
-
-    const result = await db.collection('events').updateOne(
-        { eventId : id } ,
-        { $set : data }
-    ) ;
-    return NextResponse.json(result) ;
-    
+export async function POST(request) {
+  const db = await getDatabase();
+  const data = await request.json();
+  const result = await db.collection("events").insertOne(data);
+  return NextResponse.json(result);
 }
 
-export async function DELETE(req){
+export async function PUT(req) {
+  const db = await getDatabase();
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("eventId");
+  const data = await req.json();
 
-    const { searchParams } = new URL(req.url) ;
-    const email = searchParams.get('email') ;
-    const eventId = searchParams.get('eventId') ;
-    const db = await getDatabase() ;
-    const result = await db.collection('events').deleteOne({organizerEmail : email, _id : new ObjectId(eventId)}) ;
-    return NextResponse.json(result) ;
-
+  const result = await db
+    .collection("events")
+    .updateOne({ eventId: id }, { $set: data });
+  return NextResponse.json(result);
 }
 
-
+export async function DELETE(req) {
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
+  const eventId = searchParams.get("eventId");
+  const db = await getDatabase();
+  const result = await db
+    .collection("events")
+    .deleteOne({ organizerEmail: email, _id: new ObjectId(eventId) });
+  return NextResponse.json(result);
+}
 
 // updating an event
 
-
-
-//ticket stat aggregrate api 
-
-
-
+//ticket stat aggregrate api
